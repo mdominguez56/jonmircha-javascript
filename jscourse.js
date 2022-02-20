91d00cf76320e55f02eb211c2f5715cb04931d48
Matias Dominguez Alonso <matiasdominguez3@gmail.com>
2022-02-20 19:11:51 -0300

    video10

--- jscourse.js
+++ jscourse.js
@@ -1,1 +1,35 @@
 // Las funciones son un tipo de dato complejo, considerado objeto.
+
+// Funciones declaradas
+function funcionQueDevuelveValor(){
+    return "La funcion ha retornado una cadena de texto"
+}
+let valorDeFuncion = funcionQueDevuelveValor()
+console.log(valorDeFuncion)
+
+
+// Valor por defecto de parametros de una funcion
+function saludar(nombre = "desconocido",edad = 0){
+    console.log(`Hola mi nombre es ${nombre} y tengo ${edad}`)
+}
+
+saludar("Matias Dominguez Alonso", 29)
+saludar()
+
+
+// Funciones declaradas - Puedo invocarse antes de declarada
+funcionDeclarada()
+
+function funcionDeclarada(){
+    console.log("Esto es una funcion declarada")
+}
+
+funcionDeclarada()
+
+// Funcion anonima - Solo puede invocarse luego de su definicion
+
+const funcionExpresada = function(){
+    console.log("Esto es una funcion expresada")
+}
+
+funcionExpresada()
