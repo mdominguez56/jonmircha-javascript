82f83d3fe5b56d42a0737d961da701250ee7cb4f
Matias Dominguez Alonso <matiasdominguez3@gmail.com>
2022-02-21 20:30:04 -0300

    new state class 12

--- clase13.js
+++ clase13.js
@@ -1,2 +1,24 @@
 // Class 13 - Objects
 
+const b = {}
+console.log(b)
+
+const c = new Object()
+console.log(c)
+
+const matias = {
+    name: "Matias",
+    lastname: "Dominguez Alonso",
+    age: 29,
+    hobbies: ["Run","Crossfit"],
+    married: true,
+    contact: {
+        email: "matias@gmail.com",
+        mobile: 123456789
+    },
+    fun: function(){
+        console.log("Hello")
+    }
+}
+
+// Minuto 10 del video
