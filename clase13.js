4233f773e2b65374202f59f12d207a028f843b64
Matias Dominguez Alonso <matiasdominguez3@gmail.com>
2022-02-28 19:30:50 -0300

    add y finish class 13

---
+++ clase13.js
@@ -0,0 +1,33 @@
+// Class 13 - Operators types
+
+// Operadores aritmeticos
+
+let a = 5 + 5 * (2 + 3)
+console.log(a)
+
+let b = 5 % 2 // Operador modulo, para obtener el resto de una division
+console.log(b)
+
+// Operadores racionales
+
+let c = 8 > 9
+console.log(c)
+
+// Incremento y Decremento
+let d = 1
+d += 3 // Incremento
+console.log(d)
+
+// Operador unario
+let e = 1
+e++
+console.log(e)
+
+// Operadores logicos
+// ! - Not
+// || - Or
+// && - And
+
+console.log(!true)
+console.log((9 === 9) || ("9" === 9)) // Si una condicion es true, da true como resultado
+console.log((9 === 9) && ("9" === 9)) // Para un true, necesita que todas sean true
