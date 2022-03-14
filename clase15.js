e4dc620fc282359e3869f066107ff653378517d6
Matias Dominguez Alonso <matiasdominguez3@gmail.com>
2022-03-14 08:59:04 -0300

    add class 15

--- clase15.js
+++ clase15.js
@@ -1,1 +1,36 @@
-// Hacer clase desde 0
+// Class 15 - Loops
+
+let contador = 0;
+
+while(contador < 10){
+    console.log(contador)
+    contador++;
+}
+
+do {
+    console.log("while " + contador)
+    contador++;
+} while(contador < 10)
+
+for (let i = 0; i < 10; i++){
+    console.log ("for " + i)
+}
+
+let numeros = [10,20,30,40,50,60]
+for (let i = 0; i < numeros.length; i++){
+    console.log(numeros[i])
+}
+
+let matias = {
+    nombre: "Matias",
+    apellido: "Dominguez",
+    edad: 29
+
+}
+for (const propiedad in matias) {
+    console.log(`Key: ${propiedad}, Value: ${matias[propiedad]}`)
+}
+
+for (const elemento of numeros) {
+    console.log(elemento)
+}
