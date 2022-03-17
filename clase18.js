5dd21c29e5a4e636b9611e943964ed82fbfdf937
Matias Dominguez Alonso <matiasdominguez3@gmail.com>
2022-03-17 19:13:56 -0300

    finish with class18

--- clase18.js
+++ clase18.js
@@ -1,3 +1,16 @@
 // Class 18 - Destructuring
 
-// Hacer clase desde 0
+const numbers = [0,1,2,3]
+
+// Destructuring
+const [zero,one,two,three] = numbers
+console.log(zero,one)
+
+let person = {
+    name: "Matias",
+    lastname: "Dominguez Alonso",
+    age: 29
+}
+
+let {name, lastname, age} = person
+console.log(name,lastname,age)
