0441bc70faee81f1d38335b2c73982801d1eae70
Matias Dominguez Alonso <matiasdominguez3@gmail.com>
2022-03-17 19:22:52 -0300

    finish with class19

--- clase19.js
+++ clase19.js
@@ -1,1 +1,22 @@
 // Objects
+
+let name = "Robin"
+let age = 1
+
+const dog = {
+    name: name,
+    age: age,
+    ladrar: function() {
+        console.log("Gauu Gauu")
+    }
+}
+
+console.log(dog)
+dog.ladrar()
+
+const perro = {
+    name,
+    age,
+}
+
+console.log(perro)
